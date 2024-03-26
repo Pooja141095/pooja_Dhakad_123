6a46db823998a8a0625044ac97b49e2c0892f6f4
pooja <poojadhaker1410@gmail.com>
2024-03-26 20:32:22 +0530

    my

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Routing from "./component/Routing";
+import { BrowserRouter } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+    <Routing></Routing>
+   </BrowserRouter>
+  );
+}
+
+export default App;
